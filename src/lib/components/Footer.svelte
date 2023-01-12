191b1086ed7ef1b8282ced8d7bb4bdbdfa0dee97
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-01-12 22:43:36 +0800

    :sparkles: Dummy footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import '@picocss/pico';
+</script>
+
+<footer>
+	<div>
+		<a href="https://github.com/ValentinVignal"
+			><img src="src/lib/assets/icons/github.svg" alt="Github" height="40" width="40" />
+		</a>
+		<a href="https://twitter.com/VignalValentin1">
+			<img src="src/lib/assets/icons/twitter.svg" alt="Twitter" height="40" width="40" />
+		</a>
+		<a href="https://stackoverflow.com/users/12066144/valentin-vignal">
+			<img
+				src="src/lib/assets/icons/stack-overflow.svg"
+				alt="Stack Overflow"
+				height="40"
+				width="40"
+			/>
+		</a>
+		<a href="https://discordapp.com/users/825997018605551647">
+			<img src="src/lib/assets/icons/discord.svg" alt="Discord" height="40" width="40" />
+		</a>
+		<div />
+		valentin.vignal.dev@outlook.fr
+	</div>
+	Valentin Vignal
+</footer>
