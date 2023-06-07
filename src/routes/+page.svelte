c9bf51ed65dcb7698e815f09c0e768954d57ecc0
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-07 17:32:49 +0800

    :construction: 9: Filter pokemons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,20 @@
 	import PokemonCard from './PokemonCard.svelte';
 
 	export let data: PageData;
+	let form = {
+		search: ''
+	};
+	let search = '';
+	$: selectedPokemons = data.pokemons.filter((pokemon) => {
+		return pokemon.name.toLowerCase().includes(search.toLowerCase());
+	});
 
 	$: pokemonId = $page.url.searchParams.get('pokemon-id') ?? '';
-
 	$: selectedPokemon = data.pokemons.find((pokemon) => pokemon.id === pokemonId);
+
+	const submitSearch = (event: Event) => {
+		search = form.search;
+	};
 </script>
 
 {#if selectedPokemon}
@@ -23,8 +33,13 @@
 	{/each}
 </div>
 
+<form class="search-form" on:submit={submitSearch}>
+	<input type="text" bind:value={form.search} placeholder="Pokemon Name" />
+	<input type="submit" value="Search" />
+</form>
+
 <div class="pokemons">
-	{#each data.pokemons as pokemon (pokemon.id)}
+	{#each selectedPokemons as pokemon (pokemon.id)}
 		<PokemonCard {pokemon} isInteractive={true} />
 	{/each}
 </div>
@@ -54,5 +69,27 @@
 		flex-wrap: wrap;
 		flex-direction: row;
 		justify-content: center;
+	}
+
+	.search-form {
+		display: flex;
+		justify-content: center;
+		margin: 20px 0;
+	}
+
+	.search-form input[type='text'] {
+		padding: 5px 10px;
+		border-radius: 5px;
+		border: 1px solid #333;
+		width: 200px;
+	}
+
+	.search-form input[type='submit'] {
+		padding: 5px 10px;
+		border: 1px solid #333;
+		border-radius: 5px;
+		margin-left: 10px;
+		background-color: #333;
+		color: #fff;
 	}
 </style>
