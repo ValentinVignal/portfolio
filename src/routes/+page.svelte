1cf774ebf3a9ddd4ecc5a31fce5705cc7deafe98
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-07 15:01:55 +0800

    :construction: 8: Display selected pokemon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import type { PageData } from './$types';
-	import type { Pokemon } from './+page';
 	import { generations } from './generations';
+	import PokemonCard from './PokemonCard.svelte';
 
 	export let data: PageData;
 
-	const pokemonId = $page.url.searchParams.get('pokemon-id') ?? '';
+	$: pokemonId = $page.url.searchParams.get('pokemon-id') ?? '';
 
 	$: selectedPokemon = data.pokemons.find((pokemon) => pokemon.id === pokemonId);
-	const onPokemonClick = (pokemon: Pokemon) => {
-		goto(`/?pokemon-id=${pokemon.id}`);
-	};
 </script>
 
-{$page.url.searchParams.get('pokemon-id')}
+{#if selectedPokemon}
+	<PokemonCard pokemon={selectedPokemon} />
+{/if}
 
 <div class="generations">
 	{#each generations as generation}
@@ -27,21 +25,11 @@
 
 <div class="pokemons">
 	{#each data.pokemons as pokemon (pokemon.id)}
-		<button class="pokemon" on:click={() => onPokemonClick(pokemon)}>
-			<div class="pokemon-content">
-				<img src={pokemon.image} alt={pokemon.name} />
-				{pokemon.name}
-			</div>
-			<div class="pokemon-id">{pokemon.id}</div>
-		</button>
+		<PokemonCard {pokemon} isInteractive={true} />
 	{/each}
 </div>
 
 <style>
-	button {
-		border: none;
-	}
-
 	.generations {
 		display: flex;
 		flex-direction: row;
@@ -66,30 +54,5 @@
 		flex-wrap: wrap;
 		flex-direction: row;
 		justify-content: center;
-	}
-	.pokemon {
-		width: 100px;
-		margin: 10px;
-		padding: 10px;
-		position: relative;
-		background-color: #eee;
-	}
-
-	.pokemon:hover {
-		background-color: #ddd;
-	}
-
-	.pokemon-content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-
-	.pokemon-id {
-		position: absolute;
-		top: 8px;
-		left: 8px;
-		font-size: 0.8em;
-		color: #aaa;
 	}
 </style>
---
+++ src/routes/PokemonCard.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import type { Pokemon } from './+page';
+
+	export let pokemon: Pokemon;
+	export let isInteractive: boolean = false;
+
+	const onPokemonClick = (pokemon: Pokemon) => {
+		goto(`/?pokemon-id=${pokemon.id}`);
+	};
+</script>
+
+<button class="pokemon" on:click={isInteractive ? () => onPokemonClick(pokemon) : null}>
+	<div class="pokemon-content">
+		<img src={pokemon.image} alt={pokemon.name} />
+		{pokemon.name}
+	</div>
+	<div class="pokemon-id">{pokemon.id}</div>
+</button>
+
+<style>
+	button {
+		border: none;
+	}
+	.pokemon {
+		width: 100px;
+		margin: 10px;
+		padding: 10px;
+		position: relative;
+		background-color: #eee;
+	}
+
+	.pokemon:hover {
+		background-color: #ddd;
+	}
+
+	.pokemon-content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.pokemon-id {
+		position: absolute;
+		top: 8px;
+		left: 8px;
+		font-size: 0.8em;
+		color: #aaa;
+	}
+</style>
