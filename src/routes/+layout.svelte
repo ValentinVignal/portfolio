79193e168c30a7e8499b68347a28f4fb5039e6f2
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-09 23:03:14 +0800

    :construction: Skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<div>
+	<a href="/">Home</a>
+	<a href="/education">Education</a>
+	<a href="/work-experience">Work experience</a>
+	<a href="/projects">Projects</a>
+	<a href="/contributions">Contributions</a>
+</div>
+
+<slot />
