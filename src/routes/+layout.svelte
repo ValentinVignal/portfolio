ceff91f4018f51219fa577af0472fe3e26f8d76b
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-10 01:14:38 +0800

    :lipstick: Add daisyui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,25 @@
-<div>
-	<a href="/">Home</a>
-	<a href="/education">Education</a>
-	<a href="/work-experience">Work experience</a>
-	<a href="/projects">Projects</a>
-	<a href="/contributions">Contributions</a>
+<script lang="ts">
+	import { base } from '$app/paths';
+	import '$lib/global.css';
+	const toggle = () => {
+		window.document.body.classList.toggle('dark-mode');
+	};
+</script>
+
+<div class="navbar">
+	<a href="{base}/">Home</a>
+	<a href="{base}/education">Education</a>
+	<a href="{base}/work-experience">Work experience</a>
+	<a href="{base}/projects">Projects</a>
+	<a href="{base}/contributions">Contributions</a>
 </div>
 
 <slot />
+
+<style>
+	.navbar {
+		display: flex;
+		justify-content: space-between;
+		padding: 10px;
+	}
+</style>
