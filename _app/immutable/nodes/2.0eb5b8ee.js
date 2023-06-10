4f2831143fca1d257b6dc25797d727b2bd8222eb
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-10 19:01:48 +0800

    Updates

---
+++ _app/immutable/nodes/2.0eb5b8ee.js
@@ -0,0 +1,1 @@
+import{S as o,i as l,s as i,q as r,r as c,b as m,D as a,h as p}from"../chunks/index.ff08a298.js";function u(n){let t;return{c(){t=r("Hello! I'm Valentin Vignal")},l(e){t=c(e,"Hello! I'm Valentin Vignal")},m(e,s){m(e,t,s)},p:a,i:a,o:a,d(e){e&&p(t)}}}class x extends o{constructor(t){super(),l(this,t,null,u,i,{})}}export{x as component};
