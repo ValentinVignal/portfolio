516672a958f2f9c160fc4e0feaf5276bbb52ef72
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-10 19:01:25 +0800

    :iphone: Navigation in drawer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,46 @@
 <script lang="ts">
 	import { base } from '$app/paths';
-	import '$lib/global.css';
-	const toggle = () => {
-		window.document.body.classList.toggle('dark-mode');
-	};
+	import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
+	import '../app.css';
 </script>
 
-<div class="navbar">
-	<a href="{base}/">Home</a>
-	<a href="{base}/education">Education</a>
-	<a href="{base}/work-experience">Work experience</a>
-	<a href="{base}/projects">Projects</a>
-	<a href="{base}/contributions">Contributions</a>
+<div class="drawer drawer-open">
+	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col items-center justify-center">
+		<slot />
+	</div>
+	<div class="drawer-side">
+		<label for="my-drawer-2" class="drawer-overlay" />
+
+		<ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
+			<li><a class="link" href="{base}/">Home</a></li>
+			<li><a class="link" href="{base}/education">Education</a></li>
+			<li><a class="link" href="{base}/work-experience">Work experience</a></li>
+			<li><a class="link" href="{base}/projects">Projects</a></li>
+			<li><a class="link" href="{base}/contributions">Contributions</a></li>
+			<div class="divider" />
+			<ThemeSwitcher />
+		</ul>
+	</div>
 </div>
 
-<slot />
+<div class="container">
+	<div class="navbar" />
+</div>
 
 <style>
+	a {
+		text-decoration: none;
+	}
+	.container {
+		display: flex;
+		flex-direction: row;
+		justify-items: start;
+	}
 	.navbar {
 		display: flex;
 		justify-content: space-between;
+		flex-direction: column;
 		padding: 10px;
 	}
 </style>
