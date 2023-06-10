11dad05945b9d5d6c8a138b020fb3ceaa25948d6
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-10 19:01:10 +0800

    :sparkles: Theme switcher

---
+++ src/lib/components/ThemeSwitcher.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
+	onMount(() => {
+		themeChange(false);
+	});
+</script>
+
+<select data-choose-theme class="select">
+	<option value="">Default</option>
+
+	<option value="dark">Dark</option>
+	<option value="light">Light</option>
+</select>
