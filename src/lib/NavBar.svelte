6cca4329f8c9446ed386c9023ac248513964211c
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-11 16:31:38 +0800

    :iphone: Responsive drawer

---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
+</script>
+
+<div class="drawer md:drawer-open">
+	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col items-center justify-center">
+		<label for="my-drawer-2" class="btn btn-primary drawer-button md:hidden">Open drawer</label>
+
+		<slot />
+	</div>
+	<div class="drawer-side">
+		<label for="my-drawer-2" class="drawer-overlay" />
+
+		<ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
+			<li><a class="link" href="{base}/">Home</a></li>
+			<li><a class="link" href="{base}/education">Education</a></li>
+			<li><a class="link" href="{base}/work-experience">Work experience</a></li>
+			<li><a class="link" href="{base}/projects">Projects</a></li>
+			<li><a class="link" href="{base}/contributions">Contributions</a></li>
+			<div class="divider" />
+			<ThemeSwitcher />
+		</ul>
+	</div>
+</div>
+
+<style>
+	a {
+		text-decoration: none;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,8 @@
 <script lang="ts">
-	import { base } from '$app/paths';
-	import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
+	import NavBar from '$lib/NavBar.svelte';
 	import '../app.css';
 </script>
 
-<div class="drawer drawer-open">
-	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content flex flex-col items-center justify-center">
-		<slot />
-	</div>
-	<div class="drawer-side">
-		<label for="my-drawer-2" class="drawer-overlay" />
-
-		<ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
-			<li><a class="link" href="{base}/">Home</a></li>
-			<li><a class="link" href="{base}/education">Education</a></li>
-			<li><a class="link" href="{base}/work-experience">Work experience</a></li>
-			<li><a class="link" href="{base}/projects">Projects</a></li>
-			<li><a class="link" href="{base}/contributions">Contributions</a></li>
-			<div class="divider" />
-			<ThemeSwitcher />
-		</ul>
-	</div>
-</div>
-
-<div class="container">
-	<div class="navbar" />
-</div>
-
-<style>
-	a {
-		text-decoration: none;
-	}
-	.container {
-		display: flex;
-		flex-direction: row;
-		justify-items: start;
-	}
-	.navbar {
-		display: flex;
-		justify-content: space-between;
-		flex-direction: column;
-		padding: 10px;
-	}
-</style>
+<NavBar>
+	<slot />
+</NavBar>
