79e8013e69e24f3bf2fe82e725abae1fa43df2dc
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-15 19:49:06 +0800

    :iphone: Close drawer on click

--- src/lib/components/Drawer.svelte
+++ src/lib/components/Drawer.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { base } from '$app/paths';
 	import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
+	import DrawerItem from './DrawerItem.svelte';
 </script>
 
 <div class="drawer md:drawer-open">
@@ -13,17 +13,26 @@
 
 		<ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
 			<li>
-				<label for="drawer" class="btn btn-ghost drawer-button md:hidden">
-					<a class="link" href="{base}/">Home</a></label
-				>
+				<DrawerItem href="/" text="Home" />
 			</li>
-
-			<li><a class="link" href="{base}/education">Education</a></li>
-			<li><a class="link" href="{base}/work-experience">Work experience</a></li>
-			<li><a class="link" href="{base}/projects">Projects</a></li>
-			<li><a class="link" href="{base}/contributions">Contributions</a></li>
-			<li><a class="link" href="{base}/skills">Skills</a></li>
-			<li><a class="link" href="{base}/accounts">Accounts</a></li>
+			<li>
+				<DrawerItem href="/education" text="Education" />
+			</li>
+			<li>
+				<DrawerItem href="/work-experience" text="Work experience" />
+			</li>
+			<li>
+				<DrawerItem href="/projects" text="Projects" />
+			</li>
+			<li>
+				<DrawerItem href="/contributions" text="Contributions" />
+			</li>
+			<li>
+				<DrawerItem href="/skills" text="Skills" />
+			</li>
+			<li>
+				<DrawerItem href="/accounts" text="Accounts" />
+			</li>
 			<div class="divider" />
 			<ThemeSwitcher />
 		</ul>
---
+++ src/lib/components/DrawerItem.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	export let href: string;
+	export let text: string;
+</script>
+
+<a
+	href="{base}{href}"
+	on:click={() => {
+		document.getElementById('drawer')?.click();
+	}}
+>
+	{text}
+</a>
