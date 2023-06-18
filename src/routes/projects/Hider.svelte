e2d4cdc64499f62b87d315964ead8289733a6b66
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-18 13:05:29 +0800

    :sparkles: Add details for hider

--- src/routes/projects/Hider.svelte
+++ src/routes/projects/Hider.svelte
@@ -1,28 +1,46 @@
 <div class="collapse bg-base-200">
 	<input type="checkbox" />
-	<div class="collapse-title text-xl font-medium">Hider</div>
+	<div class="collapse-title text-xl font-medium">
+		Hider
+		<article class="prose">
+			<p>A flutter application using firebase to store encrypted notes.</p>
+		</article>
+	</div>
 	<div class="collapse-content">
-		<p>A flutter application to store encrypted notes.</p>
-		<div class="actions">
-			<a href="https://github.com/ValentinVignal/hider" target="_blank" rel="noopener noreferrer">
-				<button class="btn btn-neutral">GitHub</button>
-			</a>
-			<a href="https://hider-c122b.web.app" target="_blank" rel="noopener noreferrer">
-				<button class="btn btn-neutral">Demo</button>
-			</a>
-			<a
-				href="https://play.google.com/store/apps/details?id=com.valentinvignal.hider"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<button class="btn btn-neutral">Play store</button>
-			</a>
-		</div>
+		<article class="prose">
+			<div class="actions">
+				<a href="https://github.com/ValentinVignal/hider" target="_blank" rel="noopener noreferrer">
+					<button class="btn btn-neutral">GitHub</button>
+				</a>
+				<a href="https://hider-c122b.web.app" target="_blank" rel="noopener noreferrer">
+					<button class="btn btn-neutral">Demo</button>
+				</a>
+				<a
+					href="https://play.google.com/store/apps/details?id=com.valentinvignal.hider"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<button class="btn btn-neutral">Play store</button>
+				</a>
+			</div>
+
+			<h2>Libraries</h2>
+			<ul>
+				<li><a href="https://pub.dev/packages/built_value">built_value</a></li>
+				<li><a href="https://pub.dev/packages/cloud_firestore">cloud_firestore</a></li>
+				<li><a href="https://pub.dev/packages/freezed">freezed</a></li>
+				<li><a href="https://pub.dev/packages/go_router">go_router</a></li>
+				<li><a href="https://pub.dev/packages/riverpod">riverpod</a></li>
+			</ul>
+		</article>
 	</div>
 </div>
 
 <style>
 	.actions {
 		padding: 8px 0px;
+	}
+	article > * > a {
+		text-decoration: underline;
 	}
 </style>
