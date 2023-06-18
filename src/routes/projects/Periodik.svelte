c2204337d970ee24aef57556f589c712ffd9257d
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-18 13:11:29 +0800

    :sparkles: Add libraries to periodik

--- src/routes/projects/Periodik.svelte
+++ src/routes/projects/Periodik.svelte
@@ -1,27 +1,41 @@
 <div class="collapse bg-base-200">
 	<input type="checkbox" />
-	<div class="collapse-title text-xl font-medium">Periodik</div>
+	<div class="collapse-title text-xl font-medium">
+		Periodik
+		<article class="prose">
+			<p>A flutter application to find the periodicity of some events based on the instances.</p>
+		</article>
+	</div>
 	<div class="collapse-content">
-		<p>A flutter application to find the periodicity of some events based on the instances.</p>
-		<div class="actions">
-			<a
-				href="https://github.com/ValentinVignal/periodik"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<button class="btn btn-neutral">GitHub</button>
-			</a>
-			<a href="https://period-ik.web.app" target="_blank" rel="noopener noreferrer">
-				<button class="btn btn-neutral">Demo</button>
-			</a>
-			<a
-				href="https://play.google.com/store/apps/details?id=com.valentinvignal.periodik"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<button class="btn btn-neutral">Play store</button>
-			</a>
-		</div>
+		<article class="prose">
+			<div class="actions">
+				<a
+					href="https://github.com/ValentinVignal/periodik"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<button class="btn btn-neutral">GitHub</button>
+				</a>
+				<a href="https://period-ik.web.app" target="_blank" rel="noopener noreferrer">
+					<button class="btn btn-neutral">Demo</button>
+				</a>
+				<a
+					href="https://play.google.com/store/apps/details?id=com.valentinvignal.periodik"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<button class="btn btn-neutral">Play store</button>
+				</a>
+			</div>
+
+			<h2>Libraries</h2>
+			<ul>
+				<li><a href="https://pub.dev/packages/cloud_firestore">cloud_firestore</a></li>
+				<li><a href="https://pub.dev/packages/freezed">freezed</a></li>
+				<li><a href="https://pub.dev/packages/go_router">go_router</a></li>
+				<li><a href="https://pub.dev/packages/riverpod">riverpod</a></li>
+			</ul>
+		</article>
 	</div>
 </div>
 
