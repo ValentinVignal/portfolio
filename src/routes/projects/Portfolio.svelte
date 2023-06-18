77c48ddd65504b7e0a60a77ab5724df07816676a
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-18 13:15:42 +0800

    :sparkles: Add libraries to portfolio

--- src/routes/projects/Portfolio.svelte
+++ src/routes/projects/Portfolio.svelte
@@ -1,29 +1,45 @@
 <div class="collapse bg-base-200">
 	<input type="checkbox" />
-	<div class="collapse-title text-xl font-medium">Portfolio</div>
+	<div class="collapse-title text-xl font-medium">
+		Portfolio
+		<article class="prose">
+			<p>My portfolio website made with SvelteKit.</p>
+		</article>
+	</div>
 	<div class="collapse-content">
-		<p>My portfolio website.</p>
-		<div class="actions">
-			<a
-				href="https://github.com/ValentinVignal/portfolio"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<button class="btn btn-neutral">GitHub</button>
-			</a>
-			<a
-				href="https://valentinvignal.github.io/portfolio"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<button class="btn btn-neutral">Demo</button>
-			</a>
-		</div>
+		<article class="prose">
+			<div class="actions">
+				<a
+					href="https://github.com/ValentinVignal/portfolio"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<button class="btn btn-neutral">GitHub</button>
+				</a>
+				<a
+					href="https://valentinvignal.github.io/portfolio"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<button class="btn btn-neutral">Demo</button>
+				</a>
+			</div>
+			<h2>Libraries</h2>
+			<ul>
+				<li><a href="https://daisyui.com">daisyUI</a></li>
+				<li><a href="https://tailwindcss.com/">tailwindcss</a></li>
+				<li><a href="https://www.npmjs.com/package/svelte-hero-icons">svelte-hero-icons</a></li>
+			</ul>
+		</article>
 	</div>
 </div>
 
 <style>
 	.actions {
 		padding: 8px 0px;
+	}
+
+	article > * > a {
+		text-decoration: underline;
 	}
 </style>
