9a960dc65618cf7a3008fe50a5c7b33a5bb345c1
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-21 18:50:28 +0800

    :sparkles: Add riverpod and drift

---
+++ src/routes/contributions/Drift.svelte
@@ -0,0 +1,36 @@
+<div class="collapse collapse-arrow bg-base-200">
+	<input type="checkbox" />
+	<div class="collapse-title text-xl font-medium">
+		<code>drift</code>
+		<article class="prose">
+			<p>
+				I made multiple contributions to <code>drift</code>, mostly on <code>drift_dev</code> which is
+				the code generator.
+			</p>
+		</article>
+	</div>
+	<div class="collapse-content">
+		<article class="prose">
+			<div class="actions">
+				<div class="actions">
+					<a href="https://pub.dev/packages/drift" target="_blank" rel="noopener noreferrer">
+						<button class="btn btn-neutral">pub.dev</button>
+					</a>
+					<a
+						href="https://github.com/simolus3/drift/pulls?q=author%3AValentinVignal+"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<button class="btn btn-neutral">GitHub</button>
+					</a>
+				</div>
+			</div>
+		</article>
+	</div>
+</div>
+
+<style>
+	.actions {
+		padding: 8px 0px;
+	}
+</style>
---
+++ src/routes/contributions/Riverpod.svelte
@@ -0,0 +1,43 @@
+<div class="collapse collapse-arrow bg-base-200">
+	<input type="checkbox" />
+	<div class="collapse-title text-xl font-medium">
+		<code>riverpod</code>
+		<article class="prose">
+			<p>
+				I made multiple contributions to <code>riverpod</code>, mostly around
+				<code>riverpod_graph</code>.
+			</p>
+		</article>
+	</div>
+	<div class="collapse-content">
+		<article class="prose">
+			<ul>
+				<li>
+					<a href="https://github.com/rrousselGit/riverpod/tree/master/packages/riverpod_graph"
+						><code>riverpod_graph</code></a
+					>
+				</li>
+			</ul>
+			<div class="actions">
+				<div class="actions">
+					<a href="https://pub.dev/packages/riverpod" target="_blank" rel="noopener noreferrer">
+						<button class="btn btn-neutral">pub.dev</button>
+					</a>
+					<a
+						href="https://github.com/rrousselGit/riverpod/issues?q=author%3AValentinVignal+"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<button class="btn btn-neutral">GitHub</button>
+					</a>
+				</div>
+			</div>
+		</article>
+	</div>
+</div>
+
+<style>
+	.actions {
+		padding: 8px 0px;
+	}
+</style>
