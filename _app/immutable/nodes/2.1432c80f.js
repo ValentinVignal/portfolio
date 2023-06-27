ca0fa418bf2924940e252d8c457f9364141a72e0
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-27 23:29:22 +0800

    Updates

---
+++ _app/immutable/nodes/2.1432c80f.js
@@ -0,0 +1,13 @@
+import{S as q,i as z,s as A,k as s,q as r,a as P,l,m as g,r as c,h as d,c as S,n as V,b as M,C as t,D as E}from"../chunks/index.481efb25.js";function N(C){let e,h,f,u,a,w,o,I,_,k,i,y,b,x;return{c(){e=s("article"),h=s("p"),f=r("Hello! I'm Valentin Vignal."),u=P(),a=s("p"),w=r("I'm a full-stack developer and I'm currently working at "),o=s("a"),I=r("Novade"),_=r(" in Singapore."),k=P(),i=s("p"),y=r(`I'm a dynamic person who loves algorithms. My work and projects, I've been accustomed to
+		handling diverse responsibilities and taking initiative with minimal oversight. I'm a constant
+		learner and enjoy keeping up to date with new technologies.
+		`),b=s("br"),x=r(`
+		I prioritize honest and straightforward communication over dealing with challenging office politics,
+		focusing instead on achieving tangible results rather than engaging in excessive discussions. However,
+		I do appreciate having a well-defined plan in place.`),this.h()},l(p){e=l(p,"ARTICLE",{class:!0});var n=g(e);h=l(n,"P",{});var j=g(h);f=c(j,"Hello! I'm Valentin Vignal."),j.forEach(d),u=S(n),a=l(n,"P",{});var m=g(a);w=c(m,"I'm a full-stack developer and I'm currently working at "),o=l(m,"A",{href:!0});var H=g(o);I=c(H,"Novade"),H.forEach(d),_=c(m," in Singapore."),m.forEach(d),k=S(n),i=l(n,"P",{});var v=g(i);y=c(v,`I'm a dynamic person who loves algorithms. My work and projects, I've been accustomed to
+		handling diverse responsibilities and taking initiative with minimal oversight. I'm a constant
+		learner and enjoy keeping up to date with new technologies.
+		`),b=l(v,"BR",{}),x=c(v,`
+		I prioritize honest and straightforward communication over dealing with challenging office politics,
+		focusing instead on achieving tangible results rather than engaging in excessive discussions. However,
+		I do appreciate having a well-defined plan in place.`),v.forEach(d),n.forEach(d),this.h()},h(){V(o,"href","https://www.novade.net/"),V(e,"class","prose")},m(p,n){M(p,e,n),t(e,h),t(h,f),t(e,u),t(e,a),t(a,w),t(a,o),t(o,I),t(a,_),t(e,k),t(e,i),t(i,y),t(i,b),t(i,x)},p:E,i:E,o:E,d(p){p&&d(e)}}}class B extends q{constructor(e){super(),z(this,e,null,N,A,{})}}export{B as component};
--- _app/immutable/nodes/9.19824d85.js
+++
@@ -1,1 +0,0 @@
-import{S as n,i as r,s as i,q as c,r as l,b as m,D as a,h as p}from"../chunks/index.b16ede70.js";function d(s){let t;return{c(){t=c("Novade.")},l(e){t=l(e,"Novade.")},m(e,o){m(e,t,o)},p:a,i:a,o:a,d(e){e&&p(t)}}}class f extends n{constructor(t){super(),r(this,t,null,d,i,{})}}export{f as component};
