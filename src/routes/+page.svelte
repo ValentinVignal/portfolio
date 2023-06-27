56dd422cc388c5357d5c432a78b3f1200e3ee7ca
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-27 23:28:52 +0800

    :sparkles: Add simple introduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
-Hello! I'm Valentin Vignal
+<article class="prose">
+	<p>Hello! I'm Valentin Vignal.</p>
+
+	<p>
+		I'm a full-stack developer and I'm currently working at <a href="https://www.novade.net/"
+			>Novade</a
+		> in Singapore.
+	</p>
+
+	<p>
+		I'm a dynamic person who loves algorithms. My work and projects, I've been accustomed to
+		handling diverse responsibilities and taking initiative with minimal oversight. I'm a constant
+		learner and enjoy keeping up to date with new technologies.
+		<br />
+		I prioritize honest and straightforward communication over dealing with challenging office politics,
+		focusing instead on achieving tangible results rather than engaging in excessive discussions. However,
+		I do appreciate having a well-defined plan in place.
+	</p>
+</article>
