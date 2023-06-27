ed4a24cac1c459a9aa9138c0c9cb6db35e9f506c
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-27 22:48:51 +0800

    :sparkles: Nestjs-query

---
+++ src/routes/contributions/NestJSQuery.svelte
@@ -0,0 +1,39 @@
+<div class="collapse collapse-arrow bg-base-200">
+	<input type="checkbox" />
+	<div class="collapse-title text-xl font-medium">
+		Nestjs-query
+		<article class="prose">
+			<p>
+				I made multiple contributions to <code>nestjs-query</code> mostly around the graphql API.
+			</p>
+		</article>
+	</div>
+	<div class="collapse-content">
+		<article class="prose">
+			<div class="actions">
+				<div class="actions">
+					<a
+						href="https://tripss.github.io/nestjs-query/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<button class="btn btn-neutral">Website</button>
+					</a>
+					<a
+						href="https://github.com/TriPSs/nestjs-query/pulls?q=author%3AValentinVignal+"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<button class="btn btn-neutral">GitHub</button>
+					</a>
+				</div>
+			</div>
+		</article>
+	</div>
+</div>
+
+<style>
+	.actions {
+		padding: 8px 0px;
+	}
+</style>
