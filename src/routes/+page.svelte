62218f510b0f622b51ef8212f878146bdfbf4dbc
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-06-27 23:34:27 +0800

    :recycle: Rephrase the introduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,22 @@
+<script>
+</script>
+
 <article class="prose">
 	<p>Hello! I'm Valentin Vignal.</p>
 
 	<p>
 		I'm a full-stack developer and I'm currently working at <a href="https://www.novade.net/"
 			>Novade</a
-		> in Singapore.
+		>
+		in Singapore.
+	</p>
+	<p>
+		Passionate about software development, architecture and building tools for developers. I prefer
+		quality over quantity. Most experienced Flutter, Svelte and NestJS.
 	</p>
 
 	<p>
-		I'm a dynamic person who loves algorithms. My work and projects, I've been accustomed to
-		handling diverse responsibilities and taking initiative with minimal oversight. I'm a constant
-		learner and enjoy keeping up to date with new technologies.
+		I'm a constant learner and enjoy keeping up to date with new technologies.
 		<br />
 		I prioritize honest and straightforward communication over dealing with challenging office politics,
 		focusing instead on achieving tangible results rather than engaging in excessive discussions. However,
