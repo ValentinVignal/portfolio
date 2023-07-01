e1d4d2cc845d375932f9e4cbee2b116bc8d76cc4
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-07-01 13:53:41 +0800

    :bug: Add z-10 to drawer side

--- src/lib/components/Drawer.svelte
+++ src/lib/components/Drawer.svelte
@@ -9,7 +9,7 @@
 	<div class="drawer-content flex flex-col items-center justify-start">
 		<slot />
 	</div>
-	<div class="drawer-side">
+	<div class="drawer-side z-10">
 		<label for="drawer" class="drawer-overlay" />
 
 		<ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
