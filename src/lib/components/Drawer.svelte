3ab98aac76c0932dcbde52774ecebad0adcd3609
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-07-01 15:05:15 +0800

    :bug: Make drawer scrollable

--- src/lib/components/Drawer.svelte
+++ src/lib/components/Drawer.svelte
@@ -12,32 +12,38 @@
 	<div class="drawer-side z-10">
 		<label for="drawer" class="drawer-overlay" />
 
-		<ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
+		<div class="drawer-content bg-base-200">
 			<img src={ProfilePicture} alt="Profile" />
-			<li>
-				<DrawerItem href="/" text="Home" />
-			</li>
-			<li>
-				<DrawerItem href="/education" text="Education" />
-			</li>
-			<li>
-				<DrawerItem href="/work-experience" text="Work experience" />
-			</li>
-			<li>
-				<DrawerItem href="/projects" text="Projects" />
-			</li>
-			<li>
-				<DrawerItem href="/contributions" text="Contributions" />
-			</li>
-			<li>
-				<DrawerItem href="/skills" text="Skills" />
-			</li>
-			<li>
-				<DrawerItem href="/accounts" text="Accounts" />
-			</li>
-			<div class="divider" />
-			<ThemeSwitcher />
-		</ul>
+			<ul class="menu w-80 text-base-content bg-base-200">
+				<li>
+					<DrawerItem href="/" text="Home" />
+				</li>
+				<li>
+					<DrawerItem href="/education" text="Education" />
+				</li>
+				<li>
+					<DrawerItem href="/work-experience" text="Work experience" />
+				</li>
+				<li>
+					<DrawerItem href="/projects" text="Projects" />
+				</li>
+				<li>
+					<DrawerItem href="/contributions" text="Contributions" />
+				</li>
+				<li>
+					<DrawerItem href="/skills" text="Skills" />
+				</li>
+				<li>
+					<DrawerItem href="/accounts" text="Accounts" />
+				</li>
+			</ul>
+			<div class="bg-base-200">
+				<div class="divider" />
+				<div class="theme-switcher">
+					<ThemeSwitcher />
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 
@@ -46,8 +52,28 @@
 		max-height: 200px;
 		aspect-ratio: 1;
 		height: 25%;
+		min-height: 100px;
 		align-self: center;
 		border-radius: 80%;
 		object-fit: cover;
+		margin-top: 16px;
+	}
+
+	.drawer-content {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		min-width: 200px;
+		height: 100%;
+		/* overflow-y: auto; */
+		padding: 16px 0px;
+	}
+
+	.theme-switcher {
+		display: flex;
+		justify-content: center;
+		padding-left: 8px;
+		padding-right: 8px;
+		padding-bottom: 16px;
 	}
 </style>
