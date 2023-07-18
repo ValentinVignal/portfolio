dfa0f40a84364c0afd52b38bbfcd8a495761ed8a
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-07-19 00:51:12 +0800

    Updates

---
+++ _app/immutable/nodes/2.313410b9.js
@@ -0,0 +1,13 @@
+import{S as F,i as J,s as M,k as o,q as l,a as q,l as r,m as d,r as s,h as c,c as H,n as j,b as R,C as e,D as N}from"../chunks/index.4acfd94f.js";function B(A){let t,h,m,_,n,w,p,I,y,b,g,x,k,i,S,E,P;return{c(){t=o("article"),h=o("p"),m=l("Hello! I'm Valentin Vignal."),_=q(),n=o("p"),w=l("I'm a full-stack developer and I'm currently working at "),p=o("a"),I=l("Novade"),y=l(`
+		in Singapore.`),b=q(),g=o("p"),x=l(`Passionate about software development, architecture and building tools for developers. I prefer
+		quality over quantity. Most experienced Flutter, Svelte and NestJS.`),k=q(),i=o("p"),S=l(`I'm a constant learner and enjoy keeping up to date with new technologies.
+		`),E=o("br"),P=l(`
+		I prioritize honest and straightforward communication over dealing with challenging office politics,
+		focusing instead on achieving tangible results rather than engaging in excessive discussions. However,
+		I do appreciate having a well-defined plan in place.`),this.h()},l(u){t=r(u,"ARTICLE",{class:!0});var a=d(t);h=r(a,"P",{});var V=d(h);m=s(V,"Hello! I'm Valentin Vignal."),V.forEach(c),_=H(a),n=r(a,"P",{});var v=d(n);w=s(v,"I'm a full-stack developer and I'm currently working at "),p=r(v,"A",{href:!0});var z=d(p);I=s(z,"Novade"),z.forEach(c),y=s(v,`
+		in Singapore.`),v.forEach(c),b=H(a),g=r(a,"P",{});var C=d(g);x=s(C,`Passionate about software development, architecture and building tools for developers. I prefer
+		quality over quantity. Most experienced Flutter, Svelte and NestJS.`),C.forEach(c),k=H(a),i=r(a,"P",{});var f=d(i);S=s(f,`I'm a constant learner and enjoy keeping up to date with new technologies.
+		`),E=r(f,"BR",{}),P=s(f,`
+		I prioritize honest and straightforward communication over dealing with challenging office politics,
+		focusing instead on achieving tangible results rather than engaging in excessive discussions. However,
+		I do appreciate having a well-defined plan in place.`),f.forEach(c),a.forEach(c),this.h()},h(){j(p,"href","https://www.novade.net/"),j(t,"class","prose svelte-1cz6k2f")},m(u,a){R(u,t,a),e(t,h),e(h,m),e(t,_),e(t,n),e(n,w),e(n,p),e(p,I),e(n,y),e(t,b),e(t,g),e(g,x),e(t,k),e(t,i),e(i,S),e(i,E),e(i,P)},p:N,i:N,o:N,d(u){u&&c(t)}}}class L extends F{constructor(t){super(),J(this,t,null,B,M,{})}}export{L as component};
