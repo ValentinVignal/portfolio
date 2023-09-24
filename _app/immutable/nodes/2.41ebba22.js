a84770658ef514873b915b11b0e5e502bc9730ca
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-09-24 22:33:24 +0800

    Updates

---
+++ _app/immutable/nodes/2.41ebba22.js
@@ -0,0 +1,7 @@
+import{s as c,n as s}from"../chunks/scheduler.bb130b15.js";import{S as p,i as d,g as h,s as g,h as u,y as m,c as f,x as v,f as i,k as _,a as l}from"../chunks/index.68f64671.js";function w(r){let e,o=`<p>Hello! I&#39;m Valentin Vignal.</p> <p>I&#39;m a full-stack developer and I&#39;m currently working at <a href="https://www.novade.net/">Novade</a>
+		in Singapore.</p> <p>Passionate about software development, architecture and building tools for developers. I prefer
+		quality over quantity. Most experienced Flutter, Svelte and NestJS.</p> <p>I&#39;m a constant learner and enjoy keeping up to date with new technologies.
+		<br/>
+		I prioritize honest and straightforward communication over dealing with challenging office politics,
+		focusing instead on achieving tangible results rather than engaging in excessive discussions. However,
+		I do appreciate having a well-defined plan in place.</p>`,a;return{c(){e=h("article"),e.innerHTML=o,a=g(),this.h()},l(t){e=u(t,"ARTICLE",{class:!0,"data-svelte-h":!0}),m(e)!=="svelte-153nio3"&&(e.innerHTML=o),a=f(t),v("svelte-8brg78",document.head).forEach(i),this.h()},h(){_(e,"class","prose svelte-1cz6k2f"),document.title="Valentin Vignal"},m(t,n){l(t,e,n),l(t,a,n)},p:s,i:s,o:s,d(t){t&&(i(e),i(a))}}}class x extends p{constructor(e){super(),d(this,e,null,w,c,{})}}export{x as component};
--- _app/immutable/nodes/2.63e6d93b.js
+++
@@ -1,7 +0,0 @@
-import{s as o,n as a}from"../chunks/scheduler.bb130b15.js";import{S as r,i as l,g as c,h as p,y as d,k as g,a as h,f as u}from"../chunks/index.68f64671.js";function f(i){let e,n=`<p>Hello! I&#39;m Valentin Vignal.</p> <p>I&#39;m a full-stack developer and I&#39;m currently working at <a href="https://www.novade.net/">Novade</a>
-		in Singapore.</p> <p>Passionate about software development, architecture and building tools for developers. I prefer
-		quality over quantity. Most experienced Flutter, Svelte and NestJS.</p> <p>I&#39;m a constant learner and enjoy keeping up to date with new technologies.
-		<br/>
-		I prioritize honest and straightforward communication over dealing with challenging office politics,
-		focusing instead on achieving tangible results rather than engaging in excessive discussions. However,
-		I do appreciate having a well-defined plan in place.</p>`;return{c(){e=c("article"),e.innerHTML=n,this.h()},l(t){e=p(t,"ARTICLE",{class:!0,"data-svelte-h":!0}),d(e)!=="svelte-153nio3"&&(e.innerHTML=n),this.h()},h(){g(e,"class","prose svelte-1cz6k2f")},m(t,s){h(t,e,s)},p:a,i:a,o:a,d(t){t&&u(e)}}}class _ extends r{constructor(e){super(),l(this,e,null,f,o,{})}}export{_ as component};
