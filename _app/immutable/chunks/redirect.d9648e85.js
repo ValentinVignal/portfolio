87b9e59c73b1f139af40fd4a0153a9238ac70e74
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-11-15 10:17:57 +0800

    Updates

--- _app/immutable/chunks/redirect.d9648e85.js
+++
@@ -1,1 +0,0 @@
-import"./singletons.c9061d64.js";const r=(a,s)=>{const e=new URL(a);return e.pathname=s,e},t=(a,s)=>{const e=new URL(a);return a.searchParams.getAll("skill").includes(s)?e.searchParams.delete("skill",s):e.searchParams.append("skill",s),e};export{t as a,r as c};
