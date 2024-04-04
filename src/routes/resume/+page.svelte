08c3f2a5a3e1e1b90fe69db948db1faee6d7e60c
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-04-04 09:58:53 +0800

    :alembic: Try to display the resume on mobile

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
 	import Resume from '$lib/assets/ValentinVignal.pdf';
+	import { browser } from '$app/environment';
+
+	const pdfViewerEnabled = browser && window.navigator.pdfViewerEnabled;
 </script>
 
 <svelte:head>
 	<title>Valentin Vignal - Resume</title>
 </svelte:head>
 
-<embed class="resume" src={Resume} />
+{#if pdfViewerEnabled}
+	<embed class="resume" src={Resume} />
+{:else}
+	<iframe
+		src="https://drive.google.com/viewerng/viewer?embedded=true&url={Resume}"
+		frameBorder="0"
+		scrolling="auto"
+		class="resume"
+		title="ValentinVignal.pdf"
+	></iframe>
+{/if}
 
 <style>
 	.resume {
