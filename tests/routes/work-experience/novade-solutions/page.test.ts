a05a2f1d4fbbb2efa3c54d062beabe0f90b1b7c8
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-05-03 10:56:09 +0800

    :white_check_mark: Update the tests

--- tests/routes/work-experience/novade-solutions/page.test.ts
+++ tests/routes/work-experience/novade-solutions/page.test.ts
@@ -1,13 +1,20 @@
 import { expect, test } from '@playwright/test';
 
 test('It should display the novade solutions page', async ({ page }) => {
-  await page.goto('/portfolio/work-experience/novade-solutions');
+	await page.goto('/portfolio/work-experience/novade-solutions');
 
-  // It should have the correct title.
-  await expect(page.title()).resolves.toEqual('Valentin Vignal - Novade');
+	// It should have the correct title.
+	await expect(page.title()).resolves.toEqual('Valentin Vignal - Novade');
 
-  // It should contain my achievements.
-  await expect(page.getByText("Some achievements")).toBeVisible();
+	// It should contain my achievements.
+	await expect(page.getByText('Some achievements')).toBeVisible();
 
-  await expect(page).toHaveScreenshot({ fullPage: true }); // Takes a screenshot of the page.
+	await expect(page).toHaveScreenshot({ fullPage: true }); // Takes a screenshot of the page.
+});
+
+test('It should display the novade solutions page with a tall screen', async ({ page }) => {
+	page.setViewportSize({ width: 1280, height: 2200 });
+	await page.goto('/portfolio/work-experience/novade-solutions');
+
+	await expect(page).toHaveScreenshot({ fullPage: true }); // Takes a screenshot of the page.
 });
--- tests/routes/work-experience/page.test.ts
+++ tests/routes/work-experience/page.test.ts
@@ -1,16 +1,23 @@
 import { expect, test } from '@playwright/test';
 
 test('It should display the work experiences page', async ({ page }) => {
-  await page.goto('/portfolio/work-experience');
+	await page.goto('/portfolio/work-experience');
 
-  // It should have the correct title.
-  await expect(page.title()).resolves.toEqual('Valentin Vignal - Work Experience');
+	// It should have the correct title.
+	await expect(page.title()).resolves.toEqual('Valentin Vignal - Work Experience');
 
-  // It should contain my work experience.
-  await expect(page.getByText("Novade Solutions")).toHaveCount(3);
-  await expect(page.getByText("Novade Solutions - Senior Full Stack Developer")).toBeVisible();
-  await expect(page.getByText("Novade Solutions - Software Developer")).toBeVisible();
-  await expect(page.getByText("Novade Solutions - Part time Software Developer")).toBeVisible();
+	// It should contain my work experience.
+	await expect(page.getByText('Novade Solutions')).toHaveCount(3);
+	await expect(page.getByText('Novade Solutions - Senior Full Stack Developer')).toBeVisible();
+	await expect(page.getByText('Novade Solutions - Software Developer')).toBeVisible();
+	await expect(page.getByText('Novade Solutions - Part time Software Developer')).toBeVisible();
 
-  await expect(page).toHaveScreenshot({ fullPage: true }); // Takes a screenshot of the page.
+	await expect(page).toHaveScreenshot({ fullPage: true }); // Takes a screenshot of the page.
+});
+
+test('It should display the work experiences page with a tall screen', async ({ page }) => {
+	page.setViewportSize({ width: 1280, height: 850 });
+	await page.goto('/portfolio/work-experience');
+
+	await expect(page).toHaveScreenshot({ fullPage: true }); // Takes a screenshot of the page.
 });
