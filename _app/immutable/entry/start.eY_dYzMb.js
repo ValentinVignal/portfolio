cb87651e71c657ebfc3c1488923c19ecfb1ec768
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-08-19 17:34:15 +0800

    Updates

---
+++ _app/immutable/entry/start.eY_dYzMb.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.B99-0pN-.js";export{t as start};
--- _app/immutable/nodes/2.Cq-oo8M6.js
+++
@@ -1,8 +0,0 @@
-import{s as c,n as i}from"../chunks/scheduler.DuswzE1b.js";import{S as p,i as d,e as h,s as u,c as m,k as g,f,l as v,d as s,m as w,g as r}from"../chunks/index.VWL0RkfW.js";function _(l){let e,o=`<p>Hello! I&#39;m Valentin Vignal.</p> <p>I&#39;m a full-stack developer and I&#39;m currently working at <a href="https://www.novade.net/">Novade</a>
-		in Singapore.</p> <p>Passionate about software development, architecture and building tools for developers. I prefer
-		quality over quantity. Most experienced Flutter, Svelte and NestJS.</p> <p>I&#39;m a member of the Flutter Github organization with contributor access and over 100
-		contributions.</p> <p>I&#39;m a constant learner and enjoy keeping up to date with new technologies.
-		<br/>
-		I prioritize honest and straightforward communication over dealing with challenging office politics,
-		focusing instead on achieving tangible results rather than engaging in excessive discussions. However,
-		I do appreciate having a well-defined plan in place.</p>`,a;return{c(){e=h("article"),e.innerHTML=o,a=u(),this.h()},l(t){e=m(t,"ARTICLE",{class:!0,"data-svelte-h":!0}),g(e)!=="svelte-10am9sy"&&(e.innerHTML=o),a=f(t),v("svelte-8brg78",document.head).forEach(s),this.h()},h(){w(e,"class","prose svelte-1cz6k2f"),document.title="Valentin Vignal"},m(t,n){r(t,e,n),r(t,a,n)},p:i,i,o:i,d(t){t&&(s(e),s(a))}}}class x extends p{constructor(e){super(),d(this,e,null,_,c,{})}}export{x as component};
