4d893666bbbf48a9413cc4ce47d6d34b62da361d
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-01-09 20:31:42 +0800

    :white_check_mark: Add test with big screen for contributions

--- tests/routes/contributions/page.test.ts
+++ tests/routes/contributions/page.test.ts
@@ -1,19 +1,38 @@
 import { expect, test } from '@playwright/test';
 
 test('It should display the contributions page', async ({ page }) => {
-  await page.goto('/portfolio/contributions');
+	await page.goto('/portfolio/contributions');
 
-  // It should have the correct title.
-  await expect(page.title()).resolves.toEqual('Valentin Vignal - Contributions');
+	// It should have the correct title.
+	await expect(page.title()).resolves.toEqual('Valentin Vignal - Contributions');
 
-  // It should contain the contributions.
-  await expect(page.getByText("Flutter", { exact: true })).toBeVisible();
-  await expect(page.getByText("Flutter packages", { exact: true })).toBeVisible();
-  await expect(page.getByText("go_router", { exact: true }).first()).toBeVisible();
-  await expect(page.getByText("riverpod", { exact: true }).first()).toBeVisible();
-  await expect(page.getByText("ferry", { exact: true }).first()).toBeVisible();
-  await expect(page.getByText("drift", { exact: true }).first()).toBeVisible();
-  await expect(page.getByText("Nestjs-query", { exact: true }).first()).toBeVisible();
+	// It should contain the contributions.
+	await expect(page.getByText('Flutter', { exact: true })).toBeVisible();
+	await expect(page.getByText('Flutter packages', { exact: true })).toBeVisible();
+	await expect(page.getByText('go_router', { exact: true }).first()).toBeVisible();
+	await expect(page.getByText('riverpod', { exact: true }).first()).toBeVisible();
+	await expect(page.getByText('ferry', { exact: true }).first()).toBeVisible();
+	await expect(page.getByText('drift', { exact: true }).first()).toBeVisible();
+	await expect(page.getByText('Nestjs-query', { exact: true }).first()).toBeVisible();
 
-  await expect(page).toHaveScreenshot({ fullPage: true }); // Takes a screenshot of the page.
+	await expect(page).toHaveScreenshot({ fullPage: true }); // Takes a screenshot of the page.
+});
+
+test('It should display the contributions page  with a tall screen', async ({ page }) => {
+	page.setViewportSize({ width: 1280, height: 1100 });
+	await page.goto('/portfolio/contributions');
+
+	// It should have the correct title.
+	await expect(page.title()).resolves.toEqual('Valentin Vignal - Contributions');
+
+	// It should contain the contributions.
+	await expect(page.getByText('Flutter', { exact: true })).toBeVisible();
+	await expect(page.getByText('Flutter packages', { exact: true })).toBeVisible();
+	await expect(page.getByText('go_router', { exact: true }).first()).toBeVisible();
+	await expect(page.getByText('riverpod', { exact: true }).first()).toBeVisible();
+	await expect(page.getByText('ferry', { exact: true }).first()).toBeVisible();
+	await expect(page.getByText('drift', { exact: true }).first()).toBeVisible();
+	await expect(page.getByText('Nestjs-query', { exact: true }).first()).toBeVisible();
+
+	await expect(page).toHaveScreenshot({ fullPage: true }); // Takes a screenshot of the page.
 });
