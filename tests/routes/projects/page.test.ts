c0c99642b909a1207d32058e5b346aaa3e79f3c0
ValentinVignal <valentin.vignal.dev@outlook.fr>
2026-01-26 19:35:30 +0800

    :white_check_mark: Update snapshots and tests

--- tests/routes/projects/page.test.ts
+++ tests/routes/projects/page.test.ts
@@ -11,12 +11,11 @@ test('It should display the projects page', async ({ page }) => {
 	// It should contain the project.
 	await expect(page.getByText('ntm', { exact: true })).toBeVisible();
 	await expect(page.getByText('Padelistas')).toBeVisible();
-	await expect(page.getByText('Hider')).toBeVisible();
 	await expect(page.getByText('Periodik')).toBeVisible();
 	await expect(page.getByText('Portfolio', { exact: true })).toBeVisible();
 	await expect(page.getByText('Action dart analyze')).toBeVisible();
-	await expect(page.getByText('Pyronear')).toBeVisible();
 	await expect(page.getByText('animated_collection')).toBeVisible();
+	await expect(page.getByText('Pyronear')).toBeVisible();
 	await expect(page.getByText('NotificationBlocker')).toBeVisible();
 	await expect(page.getByText('Themed color palette')).toBeVisible();
 	await expect(page.getByText('Chrome markdown emoji')).toBeVisible();
@@ -36,7 +35,6 @@ for (const [themeId, theme] of options) {
 	test(`It should display the expanded projects page - ${theme} theme`, async ({ page }) => {
 		await page.route('https://ntm-pad.web.app', (route) => route.abort());
 		await page.route('https://padelistas-app.web.app', (route) => route.abort());
-		await page.route('https://hider-c122b.web.app', (route) => route.abort());
 		await page.route('https://period-ik.web.app/', (route) => route.abort());
 
 		page.setViewportSize({ width: 1280, height: 7000 });
@@ -51,24 +49,22 @@ for (const [themeId, theme] of options) {
 		await page.getByText('Chrome markdown emoji').click({ force: true });
 		await page.getByText('Themed color palette').click({ force: true });
 		await page.getByText('NotificationBlocker').click({ force: true });
-		await page.getByText('animated_collection').click({ force: true });
 		await page.getByText('Pyronear').click({ force: true });
+		await page.getByText('animated_collection').click({ force: true });
 		await page.getByText('Action dart analyze').click({ force: true });
 		await page.getByText('Portfolio', { exact: true }).click({ force: true });
 		await page.getByText('Periodik').click({ force: true });
-		await page.getByText('Hider').click({ force: true });
 		await page.getByText('Padelistas').click({ force: true });
 		await page.getByText('ntm', { exact: true }).click({ force: true });
 
 		// It should contain the projects.
 		await expect(page.getByText('ntm', { exact: true })).toBeVisible();
 		await expect(page.getByText('Padelistas')).toBeVisible();
-		await expect(page.getByText('Hider')).toBeVisible();
 		await expect(page.getByText('Periodik')).toBeVisible();
 		await expect(page.getByText('Portfolio', { exact: true })).toBeVisible();
 		await expect(page.getByText('Action dart analyze')).toBeVisible();
-		await expect(page.getByText('Pyronear')).toBeVisible();
 		await expect(page.getByText('animated_collection')).toBeVisible();
+		await expect(page.getByText('Pyronear')).toBeVisible();
 		await expect(page.getByText('NotificationBlocker')).toBeVisible();
 		await expect(page.getByText('Themed color palette')).toBeVisible();
 		await expect(page.getByText('Chrome markdown emoji')).toBeVisible();
