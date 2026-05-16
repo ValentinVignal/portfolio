514ab3232bbc8764671941008feba2c948247d2b
ValentinVignal <valentin.vignal.dev@outlook.fr>
2026-05-16 15:55:38 +0800

    Update tests

--- tests/routes/work-experience/prudential-plc/page.test.ts
+++ tests/routes/work-experience/prudential-plc/page.test.ts
@@ -3,6 +3,7 @@ import { expect, test } from '@playwright/test';
 import { setTheme } from '../../../utils/utils';
 
 test('It should display the prudential page', async ({ page }) => {
+	await page.route('https://pruservices.prudential.com.sg/app', (route) => route.abort());
 	await page.goto('/portfolio/work-experience/prudential-plc');
 
 	// It should have the correct title.
@@ -12,10 +13,9 @@ test('It should display the prudential page', async ({ page }) => {
 });
 
 for (const [themeId, theme] of options) {
-	test(`It should display the novade solutions page with a tall screen - ${theme}`, async ({
-		page
-	}) => {
-		page.setViewportSize({ width: 1280, height: 1000 });
+	test(`It should display the prudential page with a tall screen - ${theme}`, async ({ page }) => {
+		page.setViewportSize({ width: 1280, height: 1500 });
+		await page.route('https://pruservices.prudential.com.sg/app', (route) => route.abort());
 		await page.goto('/portfolio/work-experience/prudential-plc');
 
 		await setTheme(page, themeId);
